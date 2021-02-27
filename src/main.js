4cf1f34ef01a7c020ca739edbac56c3cbfb12f56
Rafael Ramos <161553@upf.br>
2021-02-27 11:57:17 -0300

    SPA e criacao de rotas

--- src/main.js
+++ src/main.js
@@ -3,12 +3,24 @@ import App from './App.vue'
 
 // importando o módulo
 import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
+// se não é utilizado export default tem que utilizar {} no import
+import {routes} from './routes'
 
 // registrando o módulo/plugin no global view object
 Vue.use(VueResource)
+Vue.use(VueRouter)
 
+// com auxilo do VueRouter vai ser passado as rotas configuradas e ele vai devolver um roteador que será utilizado pela aplicação
+const router = new VueRouter({ 
+  routes: routes,
+  mode: 'history'
+ });
+
+// Instancia vue
 new Vue({
   el: '#app',
+  router: router,
   render: h => h(App)
   /* Mesmo que:
   render: function(h) {
